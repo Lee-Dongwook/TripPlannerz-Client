073a037be3ed970570ea8ad6165a8e534d1bb55f
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-29 17:35:46 +0900

    feat(storybook) StartPage story

---
+++ client/src/stories/startPage.stories.tsx
@@ -0,0 +1,16 @@
+import {Meta, StoryObj} from '@storybook/react';
+
+import StartPage from '@/ui/start/start';
+
+const meta: Meta<typeof StartPage> = {
+    component: StartPage,
+};
+
+export default meta;
+type Story = StoryObj<typeof StartPage>
+
+export const FirstStory: Story = {
+    args: {
+
+    },
+};
