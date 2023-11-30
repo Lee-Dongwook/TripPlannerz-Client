2020d0282ff870d7bff0de7eef68f9dc8ae75cc3
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-30 16:07:55 +0900

    fix(ui) mainPage

--- client/src/application/api/main/getEntireTripList.ts
+++ client/src/application/api/main/getEntireTripList.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 export const getEntireTripList = async(token: string | null) => {
+
+    // 서버 변경에 따른 임시 적용
+    const currentNumber = 0;
+    const order = "new";
+
     if(token){
-        const response = await axios.get(`http://localhost:8080/api/trip/entireTripList`,{
+        const response = await axios.get(`http://localhost:8080/api/trip/tripList?page=${currentNumber}&sortType=${order}&keyWord=`,{
             headers: {'Authorization': `Bearer ${token}` }
         }) 
         return response;
--- client/src/ui/main/main.tsx
+++ client/src/ui/main/main.tsx
@@ -1,58 +1,21 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Card, List, Progress, FloatButton } from "antd";
 import { QuestionCircleOutlined } from '@ant-design/icons'
 
+import { Trip } from "@/domain/TripList";
 import { getEntireTripList } from "@/application/api/main/getEntireTripList";
+
 
 
 function MainPage() {
 
   const token = useSelector((state:any) => state.token.token);
+  
+  const navigate = useNavigate();
 
-  const [travelList, setTravelList] = useState([
-  {
-    title: '서울',
-    currentNum: 1,
-    recruitNum: 3,
-    startingDate: '2023-10-20',
-    comingDate: '2023-11-11',
-  },
-  {
-    title: '부산',
-    currentNum: 2,
-    recruitNum: 5,
-    startingDate: '2023-10-20',
-    comingDate: '2023-11-11',
-  },
-  {
-    title: '대구',
-    currentNum: 3,
-    recruitNum: 8,
-    startingDate: '2023-10-20',
-    comingDate: '2023-11-11',
-  },
-  {
-    title: '인천',
-    currentNum: 5,
-    recruitNum: 6,
-    startingDate: '2023-10-20',
-    comingDate: '2023-11-11',
-  },
-  {
-    title: '대전',
-    currentNum: 1,
-    recruitNum: 5,
-    startingDate: '2023-10-20',
-    comingDate: '2023-11-11',
-  },
-  {
-    title: '제주',
-    currentNum: 4,
-    recruitNum: 5,
-    startingDate: '2023-10-20',
-    comingDate: '2023-11-11',
-  }])
+  const [travelList, setTravelList] = useState<Trip[]>([])
 
 
   const fetchData = async() => {
@@ -64,40 +27,33 @@ function MainPage() {
 
   useEffect(() => {
     fetchData();
-    console.log(token);
   }, [])
   
-  function movetoSubPage(point) {
-    window.location.href = `/search/${point}`;
+  const movetoSubPage = (num: number) => {
+    navigate(`/search/${num}`)
   }
   
   return (
     <div>
-          <List
-            grid={{ gutter: 16, column: 6 }}
-            dataSource={travelList}
-            renderItem={(item) => (
+          <List 
+              grid={{ gutter: 16, column: 6 }} 
+              dataSource={travelList} 
+              renderItem={(item) => (
               <List.Item>
-              <Card
-                onClick={(e) => movetoSubPage(item.id)}
-                style={{ width: '300px', height: '340px', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
-              >
-              <h3 style={{ color: '#1890ff', marginBottom: '10px', textAlign: 'center' ,fontWeight: 'bold', fontSize: '1.5rem' }}>
-                {item.title}
-              </h3>
-              <hr />
-              <div style={{ color: '#666', marginBottom: '10px', fontSize: '1rem' }}>
-                <strong>인원 현황: </strong>
-                  {item.currentNum} / {item.recruitNum}
-                <br />
-                <Progress percent={Math.floor((item.currentNum / item.recruitNum) * 100)} status="active" />
-              </div>
-              <div style={{ color: '#666', fontSize: '0.9rem' }}>
-                <strong>여행 기간:</strong> {item.startingDate} ~ {item.comingDate}
-              </div>
-            </Card>
+                <Card onClick={() => movetoSubPage(item.id? item.id : 0)} style={{ width: '300px', height: '340px', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                <h3 style={{ color: '#1890ff', marginBottom: '10px', textAlign: 'center' ,fontWeight: 'bold', fontSize: '1.5rem' }}>{item.title}</h3>
+                <hr />
+                <div style={{ color: '#666', marginBottom: '10px', fontSize: '1rem' }}>
+                  <strong>인원 현황: </strong> {item.currentNum} / {item.recruitNum}
+                  <br />
+                  <Progress percent={item.currentNum && item.recruitNum ? Math.floor((item.currentNum / item.recruitNum) * 100) : 0} status="active" />
+                </div>
+                <div style={{ color: '#666', fontSize: '0.9rem' }}>
+                  <strong>여행 기간:</strong> {item.goingDate} ~ {item.comingDate}
+                </div>
+                </Card>
             </List.Item>
-          )}/>
+            )}/>
       <FloatButton
         tooltip={<div>Questions</div>}
         style={{width: '60px', height: '60px'}}
