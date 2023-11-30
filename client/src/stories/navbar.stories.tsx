09c6a6ac0c30667786cc5e8ddecdbb057a209dfd
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-30 11:04:25 +0900

    feat(storybook) Current Work

---
+++ client/src/stories/navbar.stories.tsx
@@ -0,0 +1,16 @@
+import {Meta, StoryObj} from '@storybook/react';
+
+import Navbar from '@/ui/navbar/navbar';
+
+const meta: Meta<typeof Navbar> = {
+    component: Navbar,
+};
+
+export default meta;
+type Story = StoryObj<typeof Navbar>
+
+export const NavbarStory: Story = {
+    args: {
+
+    },
+};
