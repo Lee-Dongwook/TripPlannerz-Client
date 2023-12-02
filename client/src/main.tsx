3eb51f2c27d1127958598ded8ea84e4d6d363a7f
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-02 18:27:05 +0900

    refactor(redux) store import 구조 변경

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { rootReducer } from '@/store/reducer/index.ts'
+import store from '@/store/store.ts';
 import App from './App.tsx'
-
-const store = createStore(rootReducer)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
