fe1a694a6d557b938fc25b1c3bd257eb5cb8b7cd
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-02 16:05:02 +0900

    fix(ui) MyPage for currentPageComponent

--- client/src/ui/my/my.tsx
+++ client/src/ui/my/my.tsx
@@ -1,86 +1,23 @@
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
 import { Button } from "antd";
 
-import type { Member } from "@/domain/Member";
-import type { AccompanyList } from "@/domain/AccompanyList";
-
-import { getMemberTripInfo } from "@/application/api/my/getMemberTripInfo";
-import { getTripAccompanyRequestList } from "@/application/api/my/getTripAccompanyRequestList";
-import { getPaginatedTripList } from "@/application/api/my/getPaginatedTripList";
-
-import { convertPreverenceRankToTypeName } from "@/lib/info/convertPreferenceRankToTypeName";
-
-import { myCurrentRender } from "@/ui/my/myCurrentRender";
-
+import { myCurrentRender } from '@/ui/my/myCurrentRender'; 
 
 function MyPage() {
-  const token = useSelector((state:any) => state.token.token);
-
-  const [memberInfo, setMemberInfo] = useState<Member>({});
-  const [accompanyList, setAccompanyList] = useState<AccompanyList[]>([])
-  const [posts, setPosts] = useState<any[]>([]);
-  const [currentPageState, setCurrentPageState] = useState("profile");
+  const [currentPageState, setCurrentPageState] = useState<string>("profile");
   const [currentPageComponent, setCurrentPageComponent] = useState<JSX.Element>();
 
-  const handleGetMemberTripInfo = async() => {
-    const response = await getMemberTripInfo(token);
-
-    if(response.data){
-      
-      const updateMemberInfo: Record<Exclude<keyof Member,"pw">, any> = {
-        name: response.data.name,
-        gender: response.data.gender,
-        email: response.data.email,
-        types: response.data.prefereneces
-      }
-      
-      setMemberInfo((prevInfo) => ({
-        ...prevInfo,
-        updateMemberInfo
-      }))
+  const handleCurrentPageChange = () => {
+    const render = myCurrentRender({currentPageState});
+    if(render) {
+      setCurrentPageComponent(render);
     }
-  }
 
-  const handleGetTripAccompanyRequestList = async() => {
-    const response = await getTripAccompanyRequestList(token);
-
-    if(response.data){
-      const newAccompanyList = {
-        comment: response.data[0].comment,
-        comment_id: response.data[0].comment_id,
-        senderName: response.data[0].senderName,
-        tripName: response.data[0].tripName,
-        tripUUID: response.data[0].tripUUID
-      }
-
-      setAccompanyList([...accompanyList, newAccompanyList]);
-    }
-  }
-
-  const handleGetPaginatedTripList = async() => {
-    const response = await getPaginatedTripList(token, currentNumber, order);
-
-    if(response.data){
-      setPosts(response.data.content);
-    }
+    throw new Error('Page Component is not compositied');
   }
 
   useEffect(() => {
-    handleGetMemberTripInfo();
-    handleGetTripAccompanyRequestList();
-    // convertPreverenceRankToTypeName(memberInfo.types);
-  }, []);
-
-  useEffect(() => {
-    handleGetPaginatedTripList();
-  }, [currentNumber, order])
-
-  useEffect(() => {
-  const render = myCurrentRender(currentPageState)
-  if(render){
-    setCurrentPageComponent(render);  
-  }  
+    handleCurrentPageChange();
   },[currentPageState])
 
   return (
@@ -93,13 +30,13 @@ function MyPage() {
           <td>
             <div>
               <div>
-                <Button className={`buttonstyle ${currentPageState === "profile" ? "active" : ""}`} onClick={() => handlePageChange("profile")}>프로필</Button>
+                <Button className={`buttonstyle ${currentPageState === "profile" ? "active" : ""}`} onClick={() => setCurrentPageState("profile")}>프로필</Button>
                 <hr />
-                <Button className={`buttonstyle ${currentPageState === "account" ? "active" : ""}`} onClick={() => handlePageChange("account")}>정보 수정</Button>
+                <Button className={`buttonstyle ${currentPageState === "account" ? "active" : ""}`} onClick={() => setCurrentPageState("account")}>정보 수정</Button>
                 <hr />
-                <Button className={`buttonstyle ${currentPageState === "schedule" ? "active" : ""}`} onClick={() => handlePageChange("schedule")}>일정 조회</Button>
+                <Button className={`buttonstyle ${currentPageState === "schedule" ? "active" : ""}`} onClick={() => setCurrentPageState("schedule")}>일정 조회</Button>
                 <hr />
-                <Button className={`buttonstyle ${currentPageState === "schedule" ? "active" : ""}`} onClick={() => handlePageChange("withdraw")}>회원 탈퇴</Button>
+                <Button className={`buttonstyle ${currentPageState === "schedule" ? "active" : ""}`} onClick={() => setCurrentPageState("withdraw")}>회원 탈퇴</Button>
               </div>
             </div>
           </td>
--- client/src/ui/my/myCurrentRender.ts
+++ client/src/ui/my/myCurrentRender.ts
@@ -1,29 +1,20 @@
-import { renderProfilePage } from "@/ui/my/render/profile/renderProfile.tsx";
+import type { MyCurrentRenderProp } from "@/ui/my/myCurrentRenderProp.types";
+import { renderProfilePage } from "@/ui/my/render/profile/renderProfile";
 import { renderBeforeAccountPage } from "@/ui/my/render/account/renderBeforeAccount";
-import { renderAfterAccountPage } from '@/ui/my/render/account/renderAfterAccount';
+import { renderAfterAccountPage } from "@/ui/my/render/account/renderAfterAccount";
 import { renderSchedulePage } from "@/ui/my/render/schedule/renderSchedule";
 import { renderWithdrawPage } from "@/ui/my/render/withdraw/renderWithdraw";
-import type { MyCurrentRenderProp } from "@/ui/my/myCurrentRenderProp.types";
 
-export const myCurrentRender = (
-  {
-    currentPageState,
-    modifyPasswordState, 
-    renderProfile, 
-    renderBeforeAccount, 
-    renderAfterAccount, 
-    renderSchedule, 
-    renderWithdraw
-  }: MyCurrentRenderProp) => {
+export const myCurrentRender = ({ currentPageState, modifyPasswordState }: MyCurrentRenderProp) => {
   switch(currentPageState){
     case "profile":
-      return renderProfilePage(renderProfile);
+      return renderProfilePage();
     case "account":
-      return modifyPasswordState === true ? renderBeforeAccountPage(renderBeforeAccount) : renderAfterAccountPage(renderAfterAccount);
+      return modifyPasswordState === true ? renderBeforeAccountPage() : renderAfterAccountPage();
     case "schedule":
-      return renderSchedulePage(renderSchedule);
+      return renderSchedulePage();
     case "withdraw":
-      return renderWithdrawPage(renderWithdraw);
+      return renderWithdrawPage();
     default:
       return null; 
   }
