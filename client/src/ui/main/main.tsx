32a00243017290c8225b2d854091712c5bafc495
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-05 14:59:06 +0900

    feat(spinner) MainPage fetch

--- client/src/ui/main/main.tsx
+++ client/src/ui/main/main.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Card, List, Progress, FloatButton } from 'antd';
+import { Card, List, Progress, Spin, FloatButton } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 
 import { Trip } from '@/domain/TripList';
@@ -13,12 +13,14 @@ function MainPage() {
   const navigate = useNavigate();
 
   const [travelList, setTravelList] = useState<Trip[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const fetchData = async () => {
     const response = await getEntireTripList(token);
     console.log(response.data.content);
     const updateList = response.data.content;
     setTravelList(updateList);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -31,58 +33,65 @@ function MainPage() {
 
   return (
     <div>
-      <List
-        grid={{ gutter: 16, column: 6 }}
-        dataSource={travelList}
-        renderItem={(item) => (
-          <List.Item>
-            <Card
-              onClick={() => movetoSubPage(item.id ? item.id : 0)}
-              style={{
-                width: '300px',
-                height: '340px',
-                borderRadius: '10px',
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-              }}
-            >
-              <h3
+      {loading ? (
+        <Spin tip='Loading...' size='large'>
+          <List grid={{ gutter: 16, column: 6 }} />
+        </Spin>
+      ) : (
+        <List
+          grid={{ gutter: 16, column: 6 }}
+          dataSource={travelList}
+          renderItem={(item) => (
+            <List.Item>
+              <Card
+                onClick={() => movetoSubPage(item.id ? item.id : 0)}
                 style={{
-                  color: '#1890ff',
-                  marginBottom: '10px',
-                  textAlign: 'center',
-                  fontWeight: 'bold',
-                  fontSize: '1.5rem',
+                  width: '300px',
+                  height: '340px',
+                  borderRadius: '10px',
+                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
                 }}
               >
-                {item.title}
-              </h3>
-              <hr />
-              <div
-                style={{
-                  color: '#666',
-                  marginBottom: '10px',
-                  fontSize: '1rem',
-                }}
-              >
-                <strong>인원 현황: </strong> {item.currentNum} /{' '}
-                {item.recruitNum}
-                <br />
-                <Progress
-                  percent={
-                    item.currentNum && item.recruitNum
-                      ? Math.floor((item.currentNum / item.recruitNum) * 100)
-                      : 0
-                  }
-                  status='active'
-                />
-              </div>
-              <div style={{ color: '#666', fontSize: '0.9rem' }}>
-                <strong>여행 기간:</strong> {item.goingDate} ~ {item.comingDate}
-              </div>
-            </Card>
-          </List.Item>
-        )}
-      />
+                <h3
+                  style={{
+                    color: '#1890ff',
+                    marginBottom: '10px',
+                    textAlign: 'center',
+                    fontWeight: 'bold',
+                    fontSize: '1.5rem',
+                  }}
+                >
+                  {item.title}
+                </h3>
+                <hr />
+                <div
+                  style={{
+                    color: '#666',
+                    marginBottom: '10px',
+                    fontSize: '1rem',
+                  }}
+                >
+                  <strong>인원 현황: </strong> {item.currentNum} /{' '}
+                  {item.recruitNum}
+                  <br />
+                  <Progress
+                    percent={
+                      item.currentNum && item.recruitNum
+                        ? Math.floor((item.currentNum / item.recruitNum) * 100)
+                        : 0
+                    }
+                    status='active'
+                  />
+                </div>
+                <div style={{ color: '#666', fontSize: '0.9rem' }}>
+                  <strong>여행 기간:</strong> {item.goingDate} ~{' '}
+                  {item.comingDate}
+                </div>
+              </Card>
+            </List.Item>
+          )}
+        />
+      )}
       <FloatButton
         tooltip={<div>Questions</div>}
         style={{ width: '60px', height: '60px' }}
