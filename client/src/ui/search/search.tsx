a388f5d9d010ae566a6eabb804f7e65642ead815
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-08 15:37:40 +0900

    feat(ui) Search Page Sidebar

--- client/src/ui/search/search.tsx
+++ client/src/ui/search/search.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Table, Spin } from 'antd';
+import { Row, Col, Menu, Table, Spin } from 'antd';
 
 import { Trip } from '@/domain/TripList';
 import { getPaginatedTripList } from '@/application/api/search/getPaginatedTripList';
 import { Pagination } from '@/ui/search/pagination/pagination';
+
+const { Item } = Menu;
 
 function SearchPage() {
   const navigate = useNavigate();
@@ -120,19 +122,33 @@ function SearchPage() {
   }));
 
   return (
-    <div>
+    <div style={{ width: '100%', height: 'calc(100vh)', display: 'flex' }}>
       {loading ? (
         <Spin tip='loading...' size='large'>
           <Table />
         </Spin>
       ) : (
         <>
-          <Table
-            columns={tableColumns}
-            dataSource={tableData}
-            pagination={false}
-          />
-          <Pagination totalPage={5} />
+          <Row style={{ width: '100%', height: '100%' }}>
+            <Col
+              span={4}
+              style={{ backgroundColor: 'whitesmoke', padding: '16px' }}
+            >
+              <Menu mode='vertical'>
+                <Item key='new'>최신 순</Item>
+                <Item key='hits'>조회수 순</Item>
+                <Item key='favorite'>좋아요 순</Item>
+              </Menu>
+            </Col>
+            <Col span={20} style={{ padding: '16px' }}>
+              <Table
+                columns={tableColumns}
+                dataSource={tableData}
+                pagination={false}
+              />
+              <Pagination totalPage={5} />
+            </Col>
+          </Row>
         </>
       )}
     </div>
