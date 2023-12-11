d22049163c5a44471d7aa6377a1ce58aa260ea3a
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-11 15:00:05 +0900

    fix(ui) Create Travel Step 구현

--- client/src/ui/create/create.tsx
+++ client/src/ui/create/create.tsx
@@ -9,9 +9,10 @@ import {
   Input,
   InputNumber,
   Upload,
-  Table,
   Row,
   Col,
+  Progress,
+  Steps,
 } from 'antd';
 import ImgCrop from 'antd-img-crop';
 
@@ -31,11 +32,72 @@ import { UserOutlined } from '@ant-design/icons';
 
 function CreatePage() {
   const navigate = useNavigate();
-
   const token = useSelector((state: any) => state.token.token);
+  const totalSteps = 8;
 
   const [tripInfo, setTripInfo] = useState<Trip>({});
   const [image, setImage] = useState([]);
+  const [currentStep, setCurrentStep] = useState<number>(0);
+
+  const steps = [
+    {
+      id: 1,
+      title: '여행 장소 선택',
+    },
+    {
+      id: 2,
+      title: '여행 사진 업로드',
+    },
+    {
+      id: 3,
+      title: '여행 제목',
+    },
+    {
+      id: 4,
+      title: '모집 인원 수',
+    },
+    {
+      id: 5,
+      title: '모집 마감 날짜',
+    },
+    {
+      id: 6,
+      title: '여행 시작 날짜',
+    },
+    {
+      id: 7,
+      title: '여행 종료 날짜',
+    },
+    {
+      id: 8,
+      title: '여행 등록',
+    },
+  ];
+
+  const items = steps.map((item) => ({ key: item.id, title: item.title }));
+
+  const handleStepChangeToPrev = () => {
+    if (currentStep === 0) {
+      alert('처음 부분입니다.');
+    } else {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const handleStepChangeToNext = () => {
+    if (currentStep < totalSteps) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handleCascaderChange = (selectedOptions: string[]) => {
+    setTripInfo((prevInfo) =>
+      updateTripInfo(prevInfo, 'area', selectedOptions[1])
+    );
+    setTripInfo((prevInfo) =>
+      updateTripInfo(prevInfo, 'sigungu', selectedOptions[2])
+    );
+  };
 
   const onImageChange = (info) => {
     setImage(info.fileList);
@@ -66,119 +128,169 @@ function CreatePage() {
     setTripInfo((prevInfo) => updateTripInfo(prevInfo, 'comingDate', event));
   };
 
-  const handleCascaderChange = (selectedOptions: string[]) => {
-    setTripInfo((prevInfo) =>
-      updateTripInfo(prevInfo, 'area', selectedOptions[1])
-    );
-    setTripInfo((prevInfo) =>
-      updateTripInfo(prevInfo, 'sigungu', selectedOptions[2])
-    );
+  const renderStepContent = (currentStep: number) => {
+    switch (currentStep) {
+      case 0:
+        return (
+          <div
+            style={{
+              width: '40%',
+              height: '80%',
+              border: '1px solid #ddd',
+              padding: '10px',
+              margin: '10px',
+              backgroundColor: 'white',
+            }}
+          >
+            <h3>1. 여행 장소 선택</h3>
+            <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
+              <Cascader
+                onChange={handleCascaderChange}
+                size='large'
+                placeholder='지역을 선택하세요'
+                options={TripCategoryCascaderOption(
+                  majorCategories,
+                  minorCategories,
+                  subCategories
+                )}
+              />
+            </Form.Item>
+          </div>
+        );
+
+      case 1:
+        return (
+          <div
+            style={{
+              width: '40%',
+              height: '80%',
+              border: '1px solid #ddd',
+              padding: '10px',
+              margin: '10px',
+              backgroundColor: 'white',
+            }}
+          >
+            <h3>2. 여행 사진 업로드</h3>
+            <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
+              <ImgCrop rotationSlider>
+                <Upload
+                  action='https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188'
+                  listType='picture-card'
+                  fileList={image}
+                  onChange={onImageChange}
+                  onPreview={onImagePreview}
+                >
+                  {image.length < 5 && '+ Upload'}
+                </Upload>
+              </ImgCrop>
+            </Form.Item>
+          </div>
+        );
+
+      case 2:
+      case 3:
+        return (
+          <div
+            style={{
+              width: '40%',
+              height: '80%',
+              border: '1px solid #ddd',
+              padding: '10px',
+              margin: '10px',
+              backgroundColor: 'white',
+            }}
+          >
+            <h3>{currentStep === 2 ? '3. 여행 제목' : '4. 모집 인원 수 '}</h3>
+            <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
+              {currentStep === 2 ? (
+                <Input
+                  style={{ width: '200px' }}
+                  onChange={handleTripTitleChange}
+                />
+              ) : (
+                <InputNumber
+                  addonBefore={<UserOutlined />}
+                  addonAfter='명'
+                  min={1}
+                  max={10}
+                  onChange={handleTripRecuritNumChange}
+                />
+              )}
+            </Form.Item>
+          </div>
+        );
+
+      case 4:
+      case 5:
+      case 6:
+        return (
+          <div
+            style={{
+              width: '40%',
+              height: '80%',
+              border: '1px solid #ddd',
+              padding: '10px',
+              margin: '10px',
+              backgroundColor: 'white',
+            }}
+          >
+            <h3>
+              {currentStep === 4
+                ? '5. 모집 마감날짜'
+                : currentStep === 5
+                  ? '6. 여행 시작 날짜'
+                  : '7. 여행 종료 날짜'}
+            </h3>
+
+            <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
+              <DatePicker
+                onChange={(_date, dateString) =>
+                  currentStep === 4
+                    ? handleTripCloseRecruitDateChange(dateString)
+                    : currentStep === 5
+                      ? handleTripGoingDateChange(dateString)
+                      : handleTripComingDateChange(dateString)
+                }
+                placeholder={
+                  currentStep === 4
+                    ? '모집 마감 날짜'
+                    : currentStep === 5
+                      ? '가는 날 선택'
+                      : '오는 날 선택'
+                }
+              />
+            </Form.Item>
+          </div>
+        );
+
+      case 7:
+        return (
+          <div
+            style={{
+              width: '40%',
+              height: '80%',
+              border: '1px solid #ddd',
+              padding: '10px',
+              margin: '10px',
+              backgroundColor: 'white',
+            }}
+          >
+            <h2>{tripInfo.title}</h2>
+            <p>{tripInfo.content}</p>
+            <p>Capacity: {tripInfo.capacity}</p>
+            <p>
+              Starting Date:{' '}
+              {new Date(tripInfo.startingDate!).toLocaleDateString()}
+            </p>
+            <p>Area: {tripInfo.area}</p>
+            <p>Sigungu: {tripInfo.sigungu}</p>
+          </div>
+        );
+
+      default:
+        return null;
+    }
   };
-
-  const createTripTableFirstRow = [
-    {
-      title: '1) 사진 업로드',
-      dataIndex: 'imageUpload',
-      width: 200,
-      render: () => (
-        <>
-          <ImgCrop rotationSlider>
-            <Upload
-              action='https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188'
-              listType='picture-card'
-              fileList={image}
-              onChange={onImageChange}
-              onPreview={onImagePreview}
-            >
-              {image.length < 5 && '+ Upload'}
-            </Upload>
-          </ImgCrop>
-        </>
-      ),
-    },
-    {
-      title: '2) 여행 제목',
-      dataIndex: 'title',
-      width: 200,
-      render: () => (
-        <Form.Item
-          name='title'
-          style={{ display: 'flex', justifyContent: 'center' }}
-        >
-          <Input style={{ width: '200px' }} onChange={handleTripTitleChange} />
-        </Form.Item>
-      ),
-    },
-    {
-      title: '3) 모집 인원',
-      dataIndex: 'capacity',
-      width: 200,
-      render: () => (
-        <Form.Item
-          name='capacity'
-          style={{ display: 'flex', justifyContent: 'center' }}
-        >
-          <InputNumber
-            addonBefore={<UserOutlined />}
-            addonAfter='명'
-            min={1}
-            max={10}
-            onChange={handleTripRecuritNumChange}
-          />
-        </Form.Item>
-      ),
-    },
-  ];
-
-  const createTripTableSecondRow = [
-    {
-      title: '4) 모집 마감 날짜',
-      dataIndex: 'deadlineDate',
-      width: 200,
-      render: () => (
-        <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
-          <DatePicker
-            onChange={(_date, dateString) =>
-              handleTripCloseRecruitDateChange(dateString)
-            }
-            placeholder='모집 마감 날짜'
-          />
-        </Form.Item>
-      ),
-    },
-    {
-      title: '5) 여행 시작 날짜',
-      dataIndex: 'startDate',
-      width: 200,
-      render: () => (
-        <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
-          <DatePicker
-            onChange={(_date, dateString) =>
-              handleTripGoingDateChange(dateString)
-            }
-            placeholder='가는 날 선택'
-          />
-        </Form.Item>
-      ),
-    },
-    {
-      title: '6) 여행 종료 날짜',
-      dataIndex: 'endDate',
-      width: 200,
-      render: () => (
-        <Form.Item style={{ display: 'flex', justifyContent: 'center' }}>
-          <DatePicker
-            onChange={(_date, dateString) =>
-              handleTripComingDateChange(dateString)
-            }
-            placeholder='오는 날 선택'
-          />
-        </Form.Item>
-      ),
-    },
-  ];
-
-  const initialData = [{}];
 
   const handleSubmitTripInfoToServer = async () => {
     const response = await SubmitTripInfoToServer(token, image, tripInfo);
@@ -196,46 +308,47 @@ function CreatePage() {
       <Row style={{ width: '100%', height: '100%' }}>
         <SideBar />
         <Col span={20} style={{ padding: '16px' }}>
-          <h5>1. 여행 장소 선택</h5>
-          <Form>
-            <Cascader
-              onChange={handleCascaderChange}
-              size='large'
-              placeholder='지역을 선택하세요'
-              options={TripCategoryCascaderOption(
-                majorCategories,
-                minorCategories,
-                subCategories
-              )}
-            />
-          </Form>
-          <hr />
-          <h5>2. 여행 정보 입력</h5>
-          <br />
-          <Form>
-            <Table
-              columns={createTripTableFirstRow}
-              dataSource={initialData}
-              bordered
-              pagination={false}
-              rowKey={(record, index) => String(index)}
-            />
-            <Table
-              columns={createTripTableSecondRow}
-              dataSource={initialData}
-              bordered
-              pagination={false}
-              rowKey={(record, index) => String(index)}
-            />
+          <Row justify='center' align='middle' style={{ height: '10%' }}>
+            <Steps current={currentStep} items={items} />
+          </Row>
+          <Form
+            style={{
+              maxWidth: '100%',
+              height: '60%',
+              backgroundColor: '#EEEEEE',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            {renderStepContent(currentStep)}
           </Form>
           <Form.Item>
-            <Button
-              type='primary'
-              htmlType='submit'
-              onClick={handleSubmitTripInfoToServer}
-            >
-              등록
-            </Button>
+            <Row justify='center'>
+              {currentStep < 7 ? (
+                currentStep > 0 ? (
+                  <>
+                    <Button onClick={handleStepChangeToPrev}>이전</Button>
+                    <Button onClick={handleStepChangeToNext}>다음</Button>
+                  </>
+                ) : (
+                  <>
+                    <Button onClick={handleStepChangeToNext}>다음</Button>
+                  </>
+                )
+              ) : (
+                <>
+                  <Button onClick={handleStepChangeToPrev}>이전</Button>
+                  <Button
+                    type='primary'
+                    htmlType='submit'
+                    onClick={handleSubmitTripInfoToServer}
+                  >
+                    등록
+                  </Button>
+                </>
+              )}
+            </Row>
           </Form.Item>
         </Col>
       </Row>
