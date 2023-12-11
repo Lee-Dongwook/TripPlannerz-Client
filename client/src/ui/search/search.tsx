8ffaf512ad32ccff8f223af90d63521e7b5c3a73
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-12-11 21:12:26 +0900

    feat(ui) Calculate Optimize Route

--- client/src/ui/search/search.tsx
+++ client/src/ui/search/search.tsx
@@ -1,10 +1,25 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import { Row, Col, Card, Drawer, Table, Spin, Input } from 'antd';
+import {
+  Button,
+  Col,
+  Card,
+  Drawer,
+  Input,
+  Modal,
+  Radio,
+  Row,
+  Space,
+  Spin,
+  Table,
+  Timeline,
+} from 'antd';
 
 import { Trip } from '@/domain/TripList';
 import { getPaginatedTripList } from '@/application/api/search/getPaginatedTripList';
+import { getDetailTripRoute } from '@/application/api/detail/getDetailTripRoute';
+import { postStartLocationToServer } from '@/application/api/detail/postStartLocationToServer';
 import SideBar from '@/ui/sidebar/sidebar';
 
 const { Meta } = Card;
@@ -16,17 +31,68 @@ function SearchPage() {
   const searchedKeyword = searchParams.get('keyword');
 
   const [tripList, setTripList] = useState<Trip[]>([]);
-  const [selectedTrip, setSelectedTrip] = useState<Trip | null>(null);
-  const [drawerState, setDrawerState] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
 
-  const handleOpenDrawer = (trip: Trip) => {
-    setSelectedTrip(trip);
-    setDrawerState(true);
+  const [selectedTrip, setSelectedTrip] = useState<any>(null);
+  const [selectedTripUuid, setSelectedTripUuid] = useState<string>('');
+  const [selectedTripRoute, setSelectedTripRoute] = useState<any[]>();
+
+  const [startLocation, setStartLocation] = useState<string>();
+
+  const [drawerState, setDrawerState] = useState<boolean>(false);
+  const [optimizeModalState, setOptimizeModalState] = useState<boolean>(false);
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [calculateRouteLoading, setCalculateRouteLoading] =
+    useState<boolean>(false);
+
+  const [optimizeTime, setOptimizeTime] = useState<number>(0);
+
+  const handleOpenDrawer = async (record) => {
+    const selectedDetailTrip = tripList.find((trip) => trip.id === record.id);
+    if (selectedDetailTrip) {
+      if (selectedDetailTrip.uuid) {
+        setSelectedTripUuid(selectedDetailTrip.uuid);
+        const response = await getDetailTripRoute(
+          token,
+          selectedDetailTrip?.uuid
+        );
+        setSelectedTripRoute(response.data);
+        setSelectedTrip(record);
+        setDrawerState(true);
+      }
+    }
   };
 
   const handleCloseDrawer = () => {
     setDrawerState(false);
+  };
+
+  const handleOpenOptimizeModal = () => {
+    setOptimizeModalState(true);
+  };
+
+  const handleCloseOptimizeModal = () => {
+    setOptimizeModalState(false);
+  };
+
+  const handleSaveStartLocation = (e) => {
+    setStartLocation(e.target.value);
+  };
+
+  const handleSendStartLocationToServer = async () => {
+    const postToServer = {
+      name: startLocation,
+      tripUUID: selectedTripUuid,
+    };
+
+    setCalculateRouteLoading(true);
+    const response = await postStartLocationToServer(token, postToServer);
+    if (response) {
+      alert('최단 경로 계산이 완료되었습니다.');
+      setOptimizeTime(parseInt(response.data.totalDuration));
+      setCalculateRouteLoading(false);
+      setOptimizeModalState(false);
+    }
   };
 
   const handleGetPaginatedTripList = async () => {
@@ -141,34 +207,115 @@ function SearchPage() {
               >
                 {selectedTrip && (
                   <>
-                    {console.log(selectedTrip)}
                     <Card>
                       <Meta
                         title={selectedTrip.title}
                         description={
                           <>
-                            <div style={{ display: 'flex' }}>
-                              <span>
-                                <strong>여행 기간:</strong>
-                              </span>
-                              <span>
-                                {selectedTrip.startingDate &&
-                                selectedTrip.comingDate
-                                  ? `${selectedTrip.startingDate} ~ ${selectedTrip.comingDate}`
-                                  : '일정 없음'}
-                              </span>
-                            </div>
-                            <div style={{ display: 'flex' }}>
-                              <span>
-                                <strong>내용:</strong>
-                              </span>
-                              <span>
-                                {selectedTrip.content
-                                  ? selectedTrip.content
-                                  : '예시 여행입니다.'}
-                              </span>
-                            </div>
+                            <h5>
+                              모집 마감 날짜:{' '}
+                              {selectedTrip.deadline
+                                ? selectedTrip.deadline
+                                : ''}
+                            </h5>
+                            <h5>
+                              여행 기간:{' '}
+                              {selectedTrip.date
+                                ? selectedTrip.date
+                                : '일정 없음'}
+                            </h5>
+                            <h5>
+                              내용:{' '}
+                              {selectedTrip.content
+                                ? selectedTrip.content
+                                : '예시 여행입니다.'}
+                            </h5>
+                            <h5>
+                              현 인원 / 총 인원 :{' '}
+                              {selectedTrip.participants
+                                ? selectedTrip.participants
+                                : ''}
+                            </h5>
                           </>
+                        }
+                      />
+                    </Card>
+                    <Card style={{ maxHeight: '500px', overflowY: 'auto' }}>
+                      <Meta
+                        title='TimeLine'
+                        description={
+                          selectedTripRoute!.length === 0 ? (
+                            '타임라인이 없습니다.'
+                          ) : (
+                            <>
+                              <Timeline>
+                                {selectedTripRoute?.map((route, index) => (
+                                  <Timeline.Item key={index}>
+                                    {route.name}
+                                  </Timeline.Item>
+                                ))}
+                              </Timeline>
+                              <Button onClick={handleOpenOptimizeModal}>
+                                최단경로
+                              </Button>
+                              <Modal
+                                title='시작점 선택'
+                                open={optimizeModalState}
+                                onCancel={handleCloseOptimizeModal}
+                                footer={[
+                                  <Button
+                                    key='cancel'
+                                    onClick={handleCloseOptimizeModal}
+                                  >
+                                    취소
+                                  </Button>,
+                                  <Button
+                                    key='confirm'
+                                    type='primary'
+                                    onClick={handleSendStartLocationToServer}
+                                  >
+                                    확인
+                                  </Button>,
+                                ]}
+                              >
+                                {optimizeModalState && (
+                                  <Radio.Group
+                                    onChange={handleSaveStartLocation}
+                                    value={startLocation}
+                                  >
+                                    {selectedTripRoute!.map((route, index) => (
+                                      <Space direction='vertical' key={index}>
+                                        <Radio value={route.name}>
+                                          {route.name}
+                                        </Radio>
+                                      </Space>
+                                    ))}
+                                  </Radio.Group>
+                                )}
+
+                                {calculateRouteLoading && (
+                                  <>
+                                    <br />
+                                    <Spin
+                                      tip='이동시간을 계산중입니다...'
+                                      size='large'
+                                    >
+                                      <></>
+                                    </Spin>
+                                  </>
+                                )}
+                              </Modal>
+                              {optimizeTime > 0
+                                ? optimizeTime.toString() +
+                                  '분 (' +
+                                  parseInt(optimizeTime / 60) +
+                                  ' 시간 ' +
+                                  (optimizeTime -
+                                    parseInt(optimizeTime / 60) * 60) +
+                                  ' 분)'
+                                : '최단 경로 버튼을 누르면 최단 경로를 계산할 수 있습니다.'}
+                            </>
+                          )
                         }
                       />
                     </Card>
