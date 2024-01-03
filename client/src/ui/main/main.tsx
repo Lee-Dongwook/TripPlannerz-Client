615c65c9b8d17fe7c51f2c688c81b89de774c4a0
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-03 21:00:11 +0900

    fix(ui) 메인 페이지 무한 스크롤 적용

--- client/src/ui/main/main.tsx
+++ client/src/ui/main/main.tsx
@@ -1,117 +1,147 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useInfiniteQuery } from 'react-query';
 import {
   Row,
   Col,
   Card,
   List,
   Progress,
-  Spin,
   FloatButton,
   Button,
+  Spin,
 } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 
-import { Trip } from '@/domain/TripList';
 import { getEntireTripList } from '@/application/api/main/getEntireTripList';
 import SideBar from '@/ui/sidebar/sidebar';
 import styles from '@/ui/main/main.module.css';
 
 function MainPage() {
   const token = useSelector((state: any) => state.token.token);
-
   const navigate = useNavigate();
 
-  const [travelList, setTravelList] = useState<Trip[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [intersectionTarget, setIntersectionTarget] = useState(null);
 
-  const fetchData = async () => {
-    const response = await getEntireTripList(token);
-    const updateList = response.data.content;
-    setTravelList(updateList);
-    setLoading(false);
-  };
+  const { data, fetchNextPage, hasNextPage, isFetching, isError } =
+    useInfiniteQuery(
+      ['tripList', token],
+      ({ pageParam = 0 }) => getEntireTripList(token, pageParam),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          const nextPageParam =
+            lastPage.data.content.length === 0 ? false : allPages.length;
+          return nextPageParam;
+        },
+      }
+    );
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const travelList = data?.pages.flatMap((page) => page.data.content) || [];
 
   const movetoSubPage = (num: number) => {
     navigate(`/search/${num}`);
   };
 
+  const observeTarget = useCallback((node) => {
+    setIntersectionTarget(node);
+  }, []);
+
+  const observerCallback = useCallback(
+    async ([entry]) => {
+      if (entry.isIntersecting && hasNextPage) {
+        try {
+          await fetchNextPage();
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+    [fetchNextPage, hasNextPage]
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(observerCallback, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1,
+    });
+
+    if (intersectionTarget) {
+      observer.observe(intersectionTarget);
+    }
+
+    return () => {
+      if (intersectionTarget) {
+        observer.unobserve(intersectionTarget);
+      }
+    };
+  }, [intersectionTarget, observerCallback]);
+
   return (
     <div className={styles.mainContainer}>
-      {loading ? (
-        <Spin tip='Loading...' size='large'>
-          <List />
-        </Spin>
-      ) : (
-        <>
-          <Row style={{ width: '100%', height: '100%' }}>
-            <SideBar />
-            <Col span={15} style={{ padding: '16px' }}>
-              <Card>
-                <h1>여행 일정</h1>
-                <h3>동행하고 싶은 여행을 찾아보세요</h3>
-              </Card>
-              <List
-                dataSource={travelList}
-                renderItem={(item) => (
-                  <List.Item>
-                    <Card className={styles.mainCard}>
-                      <h3
-                        style={{
-                          color: '#1890ff',
-                          marginBottom: '10px',
-                          textAlign: 'center',
-                          fontWeight: 'bold',
-                          fontSize: '1.5rem',
-                        }}
-                      >
-                        {item.title}
-                      </h3>
-                      <hr />
-                      <div
-                        style={{
-                          color: '#666',
-                          marginBottom: '10px',
-                          fontSize: '1rem',
-                        }}
-                      >
-                        <strong>인원 현황: </strong> {item.currentNum} /{' '}
-                        {item.recruitNum}
-                        <br />
-                        <Progress
-                          percent={
-                            item.currentNum && item.recruitNum
-                              ? Math.floor(
-                                  (item.currentNum / item.recruitNum) * 100
-                                )
-                              : 0
-                          }
-                          status='active'
-                        />
-                      </div>
-                      <div style={{ color: '#666', fontSize: '0.9rem' }}>
-                        <strong>여행 기간:</strong> {item.startingDate} ~{' '}
-                        {item.comingDate}
-                      </div>
-                      <br />
-                      <Button
-                        onClick={() => movetoSubPage(item.id ? item.id : 0)}
-                      >
-                        일정 상세 확인하기
-                      </Button>
-                    </Card>
-                  </List.Item>
-                )}
-              />
-            </Col>
-          </Row>
-        </>
-      )}
+      <Row style={{ width: '100%', height: '100%' }}>
+        <SideBar />
+        <Col span={15} style={{ padding: '16px' }}>
+          {isError && <div>오류가 발생하였습니다.</div>}
+          <Card>
+            <h1>여행 일정</h1>
+            <h3>동행하고 싶은 여행을 찾아보세요</h3>
+          </Card>
+          <List
+            dataSource={travelList}
+            renderItem={(item) => (
+              <List.Item>
+                <Card className={styles.mainCard}>
+                  <h3
+                    style={{
+                      color: '#1890ff',
+                      marginBottom: '10px',
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      fontSize: '1.5rem',
+                    }}
+                  >
+                    {item.title}
+                  </h3>
+                  <hr />
+                  <div
+                    style={{
+                      color: '#666',
+                      marginBottom: '10px',
+                      fontSize: '1rem',
+                    }}
+                  >
+                    <strong>인원 현황: </strong> {item.currentNum} /{' '}
+                    {item.recruitNum}
+                    <br />
+                    <Progress
+                      percent={
+                        item.currentNum && item.recruitNum
+                          ? Math.floor(
+                              (item.currentNum / item.recruitNum) * 100
+                            )
+                          : 0
+                      }
+                      status='active'
+                    />
+                  </div>
+                  <div style={{ color: '#666', fontSize: '0.9rem' }}>
+                    <strong>여행 기간:</strong> {item.startingDate} ~{' '}
+                    {item.comingDate}
+                  </div>
+                  <br />
+                  <Button onClick={() => movetoSubPage(item.id ? item.id : 0)}>
+                    일정 상세 확인하기
+                  </Button>
+                </Card>
+              </List.Item>
+            )}
+          />
+          {isFetching && <Spin tip='Loading...' size='large' />}
+          <div ref={observeTarget} />
+        </Col>
+      </Row>
       <FloatButton
         tooltip={<div>Questions</div>}
         style={{ width: '60px', height: '60px' }}
