c9c666a844ed45254ff9630f3abd363ae16ad12c
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-26 11:10:36 +0900

    feat : redux-toolkit MiddleWare 적용

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
+import { 
+    persistReducer, 
+    FLUSH, 
+    REHYDRATE, 
+    PAUSE, 
+    PERSIST,
+    PURGE, 
+    REGISTER 
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage/session';
+import logger from 'redux-logger';
+
 import tokenReducer from '@/store/token';
 import preferenceTypeReducer from '@/store/preferenceTypes';
 import notificationReducer from '@/store/notification';
@@ -21,8 +31,15 @@ const rootReducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: persistedReducer
-});
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    }).concat(logger),
+    devTools: process.env.NODE_ENV !== 'production'
+},
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
