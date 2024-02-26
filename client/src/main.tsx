1ef64637845cbdd48c4c397ffaf592c5e9b646f6
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-26 10:53:22 +0900

    feat : redux-persist 적용하여 token SessionStorage 저장

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import store from '@/store/store.ts';
 import App from './App.tsx';
 
+const persistor = persistStore(store);
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </PersistGate>
   </Provider>
 );
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,14 +1,27 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage/session';
 import tokenReducer from '@/store/token';
 import preferenceTypeReducer from '@/store/preferenceTypes';
 import notificationReducer from '@/store/notification';
 
+const persistConfig = {
+    key: "root",
+    storage,
+    whiteList: ['token']
+}
+
+const rootReducer = combineReducers({
+    token: tokenReducer,
+    types: preferenceTypeReducer,
+    notification: notificationReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: {
-        token: tokenReducer,
-        types: preferenceTypeReducer,
-        notification: notificationReducer
-    }
+    reducer: persistedReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
