7768ba2eb843b6000eb39c12f5f30a2e8a6e1f8d
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-03-01 13:58:19 +0900

    feat : 날씨정보 한국어 번역 리스트 업데이트

--- client/src/lib/weather/weather.tsx
+++ client/src/lib/weather/weather.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Timer from '../timer/timer';
+import { KoreanWeatherDescriptionList } from '../info/KoreanWeatherDescriptionList';
 
 const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
@@ -15,11 +16,14 @@ const weather = () => {
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
       );
+
+      const weatherDescription = KoreanWeatherDescriptionList[response.data.weather[0].id];
       const digitTemp = Math.round(response.data.main.temp);
       const weatherIcon = response.data.weather[0].icon;
       const weatherIconAddress = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
 
-      setCityName(response.data.name);
+      setCityName(response.data.name.split('-')[0]);
+      setDescription(weatherDescription);
       setTemp(digitTemp);
       setIcon(weatherIconAddress);
     } catch (error) {
@@ -29,20 +33,21 @@ const weather = () => {
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude;
-      let lng = position.coords.longitude;
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
       getWeather(lat, lng);
     });
   }, []);
 
   return (
-    <div className='weather-card border rounded-lg bg-gray-100 mt-3 h-1/4 p-4'>
+    <div className='weather-card border rounded-lg bg-gray-100 mt-3 h-1/3 p-4'>
       <Timer />
-      <div className='flex items-center justify-between mb-4'>
+      <div className='flex items-center justify-center mb-4'>
         <span className='text-lg font-bold'>{cityName}</span>
       </div>
       <img src={icon} alt='weather icon' className='w-12 h-12 mx-auto mb-4' />
-      <div className='text-xl font-bold text-center mb-4'>{temp}°C</div>
+      <div className='text-xl font-bold text-center mb-4'>{temp} °C</div>
+      <div className='text-l font-bold text-center mb-4'>{description}</div>
     </div>
   );
 };
