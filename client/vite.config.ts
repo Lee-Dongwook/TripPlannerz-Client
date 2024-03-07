6b31705838878d461153744070e34398f12704fa
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-03-07 22:20:39 +0900

    build : vite config 설정 변경

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,35 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const isProduction = mode === 'production';
+
+  const devEnv = loadEnv(mode, process.cwd(), '');
+  const prodEnv = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
-    define: {
-      'process.env': env,
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name!.split('.').at(1) as string;
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'img';
+            }
+            return `assets/${extType}/[name]-[hash][extname]`;
+          },
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+        },
+      },
     },
-    plugins: [react(), WindiCSS()],
+    define: {
+      'process.env': isProduction ? prodEnv : devEnv,
+    },
+    plugins: [react(), WindiCSS(), tsconfigPaths()],
     server: {
       host: true,
       port: 3000,
