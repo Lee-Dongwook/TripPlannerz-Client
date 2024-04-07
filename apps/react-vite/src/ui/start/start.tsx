6a151360b4d032722a9177a3e600492e78dfe096
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-04-07 22:37:34 +0900

    feat : 카카오 소셜 로그인 구현

--- apps/react-vite/src/ui/start/start.tsx
+++ apps/react-vite/src/ui/start/start.tsx
@@ -151,6 +151,12 @@ function StartPage() {
               handleSendEmailCodeToServer,
             }}
           />
+          <button
+            type='button'
+            className='w-full block bg-yellow-200 hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 mt-6 border border-gray-300'
+          >
+            카카오 소셜 로그인
+          </button>
         </div>
       </div>
     </section>
