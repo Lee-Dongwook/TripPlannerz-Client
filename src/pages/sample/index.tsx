e38f31aee598b6b50a78243ccd51786908493c42
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-04-12 00:51:15 +0900

    feat : React-Hook-Form 도입 init

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -3,63 +3,32 @@ import { useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import Layout from "@/components/layout";
+import { type RootState } from "@/store/store";
 
-const index = () => {
-  const token = useSelector((state: any) => state.token.token);
+interface FormValues {
+  title: string;
+  capacity: number;
+  closeRecruitDate: string;
+  goingDate: string;
+  comingDate: string;
+  area: string;
+  sigungu: string;
+  tripImage: string[];
+}
 
-  const { register, handleSubmit } = useForm();
+const Index = () => {
+  const token = useSelector((state: RootState) => state.token.token);
 
-  const [title, setTitle] = useState<string>("");
-  const [capacity, setCapacity] = useState<number>(0);
-  const [closeRecruitDate, setCloseRecruitDate] = useState("");
-  const [goingDate, setGoingDate] = useState("");
-  const [comingDate, setComingDate] = useState("");
-  const [area, setArea] = useState("");
-  const [sigungu, setSigungu] = useState("");
-  const [tripImage, setTripImage] = useState<any[]>([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
 
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-  };
+  const [tripImage, setTripImage] = useState<FileList | null>(null);
 
-  const handleCapacityChange = (event) => {
-    setCapacity(event);
-  };
-
-  const handleCloseRecruitDate = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setCloseRecruitDate(event.target.value);
-  };
-
-  const handleGoingDate = (event) => {
-    setGoingDate(event);
-  };
-
-  const handleComingDate = (event) => {
-    setComingDate(event);
-  };
-
-  const handleTripImageChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const files = Array.from(event.target.files).slice(
-        0,
-        5 - tripImage.length
-      );
-
-      const newImageFilesWithPreview = files.map((file) => ({
-        file,
-        preview: URL.createObjectURL(file),
-      }));
-
-      setTripImage((prevImage) => [...prevImage, ...newImageFilesWithPreview]);
-    }
-  };
-
-  const onSubmit = () => {
-    const contentsData = {
+  const onFormSubmit = handleSubmit(
+    ({
       title,
       capacity,
       closeRecruitDate,
@@ -67,32 +36,111 @@ const index = () => {
       comingDate,
       area,
       sigungu,
-    };
+    }) => {
+      if (
+        !tripImage ||
+        !title ||
+        !capacity ||
+        !closeRecruitDate ||
+        !goingDate ||
+        !comingDate ||
+        !area ||
+        !sigungu
+      ) {
+        alert("모든 항목을 입력해주세요.");
+        return;
+      }
 
-    const formData = new FormData();
-    formData.append("image", tripImage[0].originFileObj);
-    formData.append(
-      "contentsData",
-      new Blob([JSON.stringify(contentsData)], { type: "application/json" })
-    );
+      const formData = new FormData();
+      formData.append("image", tripImage[0]);
+      formData.append(
+        "contentsData",
+        new Blob(
+          [
+            JSON.stringify({
+              title,
+              capacity,
+              closeRecruitDate,
+              goingDate,
+              comingDate,
+              area,
+              sigungu,
+            }),
+          ],
+          { type: "application/json" }
+        )
+      );
 
-    axios.post("http://localhost:8080/api/trip/create", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  };
+      axios
+        .post("http://localhost:8080/api/trip/create", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          alert("여행이 생성되었습니다!");
+          console.log(response);
+          window.location.href = "/main";
+        })
+        .catch((error) => {
+          alert("오류가 발생하였습니다.");
+          console.error(error);
+        });
+    }
+  );
 
   return (
     <Layout>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" onChange={handleTitleChange} />
-        <input type="number" onChange={handleCapacityChange} />
-        <input type="file" onChange={handleTripImageChange} />
+      <form onSubmit={onFormSubmit}>
+        <input {...register("tripImage")} type="file" />
+        {errors.tripImage && <p>여행 일정과 관련된 이미지가 필요합니다.</p>}
+
+        <input
+          {...register("title")}
+          type="text"
+          placeholder="여행 일정을 입력해주세요."
+        />
+        {errors.title && <p>여행 일정 제목이 필요합니다.</p>}
+
+        <input
+          {...register("capacity")}
+          type="number"
+          placeholder="동행할 인원 수를 지정해주세요."
+        />
+        {errors.capacity && <p>동행할 인원 수가 필요합니다.</p>}
+
+        <input
+          {...register("closeRecruitDate")}
+          type="date"
+          placeholder="모집 마감 날짜를 지정해주세요."
+        />
+        {errors.closeRecruitDate && <p>모집 마감 날짜가 필요합니다.</p>}
+
+        <input
+          {...register("goingDate")}
+          type="date"
+          placeholder="여행 시작 날짜를 입력해주세요."
+        />
+        {errors.goingDate && <p>여행 시작 날짜가 필요합니다.</p>}
+
+        <input
+          {...register("comingDate")}
+          type="date"
+          placeholder="여행 종료 날짜를 입력해주세요."
+        />
+        {errors.comingDate && <p>여행 종료 날짜가 필요합니다.</p>}
+
+        <select {...register("area")}>
+          <option value=""></option>
+        </select>
+
+        <select {...register("sigungu")}>
+          <option value=""></option>
+        </select>
       </form>
     </Layout>
   );
 };
 
-export default index;
+export default Index;
