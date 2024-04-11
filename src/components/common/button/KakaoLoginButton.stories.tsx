c0023cb830129f3df8928b3bcd8c596243e9eb88
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-04-11 16:15:55 +0900

    feat : tailwindcss 적용

---
+++ src/components/common/button/KakaoLoginButton.stories.tsx
@@ -0,0 +1,9 @@
+import KakaoLoginButton from "./KakaoLoginButton";
+
+export default {
+  component: KakaoLoginButton,
+  title: "카카오 소셜 로그인 버튼",
+  tags: ["autodocs"],
+};
+
+export const Primary = {};
